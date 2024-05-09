a4ae627f1d4ba269becba725f9d1ccb154332a9f
Ruben Orea Gorriz <ruorgo@hotmail.com>
2024-05-09 13:24:38 +0200

    refactor queryhandler

--- LT.core/Handlers/AuthenticateQueryHandler.cs
+++ LT.core/Handlers/AuthenticateQueryHandler.cs
@@ -30,34 +30,43 @@ namespace LT.core.Handlers.Score
         private readonly RoleManager<IdentityRole> _roleManager = roleManager;
         private readonly AppSettings _appSettings = appSettings;
 
-        
-
         public async Task<Result<JwtSecurityToken, EntityErrorDto>> Handle(AuthenticateQuery request, CancellationToken cancellationToken)
         {
             var user = await _userManager.FindByNameAsync(request.GetEntity().Username!);
             if (user != null && await _userManager.CheckPasswordAsync(user, request.GetEntity().Password!))
             {
-                var userRoles = await _userManager.GetRolesAsync(user);
-
-                var authClaims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, user.UserName!),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                };
-
-                foreach (var userRole in userRoles)
-                {
-                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
-                }
-
-                var token = CreateToken(authClaims);
-
-                return token;
+                return await CreateToken(user);
             }
-            var threadId = Thread.CurrentThread.ManagedThreadId;
-            return new EntityErrorDto(LogLevel.Information.ToString(), threadId, 0, nameof(Handle), $"Unauthorized", null, null);
+            return CreateErrorResponse();
         }
+        private async Task<JwtSecurityToken> CreateToken(IdentityUser user)
+        {
+            var authClaims = GetInitialClaims(user.UserName!);
 
+            authClaims.AddRange(await GetClaimsByRole(user));
+
+            var token = CreateToken(authClaims);
+
+            return token;
+        }
+        private List<Claim> GetInitialClaims(string userName)
+        {
+            return new List<Claim>
+                {
+                    new(ClaimTypes.Name, userName),
+                    new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+        }
+        private async Task<List<Claim>> GetClaimsByRole(IdentityUser user)
+        {
+            List<Claim> claims = new List<Claim>();
+            var userRoles = await _userManager.GetRolesAsync(user);
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+            return claims;
+        }
         private JwtSecurityToken CreateToken(List<Claim> authClaims)
         {
             var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.GetKey()!));
@@ -71,6 +80,11 @@ namespace LT.core.Handlers.Score
                 );
 
             return token;
+        }
+        private EntityErrorDto CreateErrorResponse()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            return new EntityErrorDto(LogLevel.Information.ToString(), threadId, 0, nameof(Handle), $"Unauthorized", null, null);
         }
     }
 }
