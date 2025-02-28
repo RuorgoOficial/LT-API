e337f4000efca86d63fde169de3c9929ff577ddb
RuorgoOficial <148331999+RuorgoOficial@users.noreply.github.com>
2025-02-28 10:35:35 +0100

    Add logs

--- LT.api/Application/Handlers/ScoreQueryHandler.cs
+++ LT.api/Application/Handlers/ScoreQueryHandler.cs
@@ -26,7 +26,8 @@ namespace LT.api.Application.Handlers
             IMessageBus messageBus,
             IHttpRepository<EntityScoreDto> httpRepository,
             IRabbitMQMessageSender rabbitMQScoreMessageSender,
-            IConfiguration configuration
+            IConfiguration configuration,
+            ILogger<ScoreQueryHandler> logger
         ) :
         IRequestHandler<InsertCommand<EntityScoreDto>, int>,
         IRequestHandler<InsertServiceBusCommand<EntityScoreDto>>,
@@ -44,28 +45,33 @@ namespace LT.api.Application.Handlers
         private readonly IHttpRepository<EntityScoreDto> _httpRepository = httpRepository;
         private readonly IRabbitMQMessageSender _rabbitMQScoreMessageSender = rabbitMQScoreMessageSender;
         private readonly IConfiguration _configuration = configuration;
+        private readonly ILogger<ScoreQueryHandler> _logger = logger;
 
         public async Task<int> Handle(InsertCommand<EntityScoreDto> request, CancellationToken cancellationToken)
         {
             try
             {
+                _logger.LogInformation($"{nameof(InsertCommand<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
                 var entity = _mapper.Map<EntityScore>(request.GetEntity());
                 await _dal.Add(entity);
                 return await _unitOfWork.SaveChangesAsync(cancellationToken);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
                 return -1;
             }
         }
         public async Task Handle(InsertServiceBusCommand<EntityScoreDto> request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(InsertServiceBusCommand<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var queueName = _configuration.GetSection("RabbitMQSettings:ScoreQueueName").Value;
             _rabbitMQScoreMessageSender.SendMessage(request.GetEntity(), queueName!);
             await Task.CompletedTask;
         }
         public async Task<int> Handle(UpdateCommand<EntityScoreDto> request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(UpdateCommand<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var entity = _mapper.Map<EntityScore>(request.GetEntity());
             await _dal.Update(entity);
             return await _unitOfWork.SaveChangesAsync(cancellationToken);
@@ -73,6 +79,7 @@ namespace LT.api.Application.Handlers
         }
         public async Task<int> Handle(DeleteCommand<EntityScoreDto> request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(DeleteCommand<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var entity = _mapper.Map<EntityScore>(request.GetEntity());
             await _dal.Remove(entity);
             return await _unitOfWork.SaveChangesAsync(cancellationToken);
@@ -80,16 +87,19 @@ namespace LT.api.Application.Handlers
         }
         public async Task<IEnumerable<EntityScoreDto>> Handle(GetQuery<EntityScoreDto> request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(GetQuery<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var entities = await _dal.GetAllAsync(cancellationToken);
             return entities.Select(e => _mapper.Map<EntityScoreDto>(e));
         }
         public async Task<EntityScoreDto> Handle(GetQueryById<EntityScoreDto> request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(GetQueryById<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var entity = await _dal.GetByIdAsync(request.GetId(), cancellationToken);
             return _mapper.Map<EntityScoreDto>(entity);
         }
         public async Task<Result<IEnumerable<EntityScoreDto>, EntityErrorResponseDto<string>>> Handle(GetScoreQuery request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(GetScoreQuery)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var entities = await _dal.GetAllAsync(cancellationToken);
             var threadId = Thread.CurrentThread.ManagedThreadId;
             if (entities.Count() == 0)
@@ -99,6 +109,7 @@ namespace LT.api.Application.Handlers
         }
         public async Task<EntityScoreDto> Handle(GetHttpQuery<EntityScoreDto> request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation($"{nameof(GetHttpQuery<EntityScoreDto>)} - {Newtonsoft.Json.JsonConvert.SerializeObject(request)}");
             var entity = await _httpRepository.GetAsync(request.GetId());
             return entity;
         }
